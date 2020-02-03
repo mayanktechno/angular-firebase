e40934ed5ecba39945dbd336ad549dbbe8952520
mayank.kushwaha <mayank.kushwaha@appinventiv.com>
2020-02-03 23:50:16 +0530

    new branch created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase ,'angularfs'),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase :{
+    apiKey: "AIzaSyB6-ott6vo3FjG19et9Bx5X6AuK5W_IziU",
+    authDomain: "myproject-6ac84.firebaseapp.com",
+    databaseURL: "https://myproject-6ac84.firebaseio.com",
+    projectId: "myproject-6ac84",
+    storageBucket: "myproject-6ac84.appspot.com",
+    messagingSenderId: "99325092744",
+    appId: "1:99325092744:web:1e27fbd4b111d5e496503d",
+    measurementId: "G-GJYHWY8CQK"
+  }
 };
 
 /*
